6102ae65725d64a817c6eb20ff250dec71a85eb0
Gabriel Mornat <gabriel.mornat@gmail.com>
2013-09-27 21:36:51 +0200

    Choosing if Bootloader is Unlocked

--- cminstaller.bat
+++ cminstaller.bat
@@ -5,6 +5,13 @@ echo °                               cminstaller                             A�
 echo °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°�
 echo Are you sure to flash CyanogenMod ?
 pause
+echo IS YOUR BOOTLOADER UNLOCKED ?
+echo 1-- YES
+echo 2-- NO
+set /p choix=Que voulez vous faire ?
+if "%choix%"=="1" goto :driverstest
+if "%choix%"=="2" goto :no
+:no
 echo Turn on your device and activate USB debuging in Developers Options
 pause
 adb devices
@@ -40,3 +47,28 @@ adb sideload gapps.zip
 echo COOL ! Now CyanogenMod is installed :)
 echo Reboot your device with commands in the recovery mode you are :)
 echo WELCOME TO CYANOGENMOD !
+goto :exit
+
+:yes
+echo Waiting to activate USB debuging and continue
+pause
+adb reboot bootloader
+echo When you're on Fastboot Mode , continue the script
+fastboot flash recovery recovery.img
+echo If the flash is success , then you can boot in recovery with Volume keys
+echo and select it with POWER button , when you are in your favorite recovery ,
+echo continue the script .
+pause
+echo With your recovery , wipe Data , Cache and Dalvik and then ,
+echo start the ADB sideload mode :) and continue !
+pause
+adb sideload cm.zip
+echo If you get out ADB sideload mode after a first flash success , restart it
+echo and continue
+pause
+adb sideload gapps.zip
+echo COOL ! Now CyanogenMod is installed :)
+echo Reboot your device with commands in the recovery mode you are :)
+echo WELCOME TO CYANOGENMOD !
+
+:exit
