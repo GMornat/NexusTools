1369e41595c8bbf0c53c4accf79725ed71e0d990
attelaut <attelaut@gmail.com>
2013-10-04 21:54:03 +0300

    Create an experimental bash script for linux. Currently untested.

---
+++ cminstaller/experimentalcminstaller.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+function start {
+echo "--------------EXPERIMENTAL SCRIPT FOR LINUX, MIGHT NOT WORK---------------"
+echo "--------------------------------------------------------------------------"
+echo "-                               cminstaller                              -"
+echo "--------------------------------------------------------------------------"
+echo " "
+echo "NOTE: Use this script at your own risk!"
+echo "We are not responsible for dead kittens, any injuries or broken devices!"
+echo "Use this script only for Nexus devices. Other devices may not work or in worst case brick!"
+echo " "
+echo "This script needs root permissions. Please type in your password when prompted."
+echo " "
+echo "Are you sure to flash CyanogenMod?"
+read -p "Press enter to continue..."
+checkfiles
+}
+
+function checkfiles {
+if [ ! -f adb ]
+then
+	echo "adb is missing."
+	missingfile
+fi
+if [ ! -f fastboot ]
+then
+	echo "fastboot is missing."
+	missingfile
+fi
+if [ ! -f cm.zip ]
+then
+	echo "cm.zip is missing."
+	missingfile
+fi
+if [ ! -f recovery.img ]
+then
+	echo "recovery.img is missing."
+	missingfile
+fi
+isunlocked
+}
+
+function missingfile {
+echo "Please place it in the same directory with this file and continue."
+read -p "Press enter to continue..."
+checkfiles
+}
+
+function isunlocked {
+sudo chmod a+x adb
+sudo chmod a+x fastboot
+echo "Is your devices bootloader unlocked?"
+echo "1 -- YES"
+echo "2 -- NO"
+choix="Notset"
+echo -n "Type 1 or 2: "
+read choix
+if [ $choix == "1" ]
+then
+	fastboot
+elif [ $choix == "2" ]
+	unlock
+else
+	echo "Invalid  choice. Type only 1 or 2."
+	isunlocked
+fi
+}
+
+function unlock {
+echo "Turn on your device and activate USB debuging in Developers Options"
+read -p "Press enter to continue..."
+sudo ./adb devices
+echo "Continue if you see your device in list above."
+read -p "Press enter to continue..."
+sudo ./adb reboot bootloader
+echo "Continue when your device is in Fastboot mode."
+read -p "Press enter to continue..."
+sudo ./fastboot devices
+echo "Continue if you see your device in list above."
+read -p "Press enter to continue..."
+echo "Your device will be now unlocked"
+echo "NOTE: All your data will be lost now!"
+read -p "Press enter to continue..."
+echo "Unlocking device..."
+sudo ./fastboot oem unlock
+echo "Follow the instructions on the devices screen to unlock your bootloader."
+echo "Use volume buttons to select and confirm with power button."
+echo "Continue when your device has been unlocked successfully."
+read -p "Press enter to continue..."
+echo "Check if the device has rebooted to android."
+echo 'If not, reboot it manually by selecting "Reboot" in bootloader'
+echo "with volume keys and confirm with power button."
+echo "Continue when the device has rebooted."
+read -p "Press enter to continue..."
+echo "Turn on USB debugging again since all data was lost when the device was unlocked"
+sudo ./adb devices
+echo "Continue if you see your device in list above."
+read -p "Press enter to continue..."
+sudo ./adb reboot bootloader
+echo "Continue when your device is in Fastboot mode."
+read -p "Press enter to continue..."
+sudo ./fastboot devices
+echo "Continue if you see your device in list above."
+read -p "Press enter to continue..."
+echo "Look at the last line on your devices screen."
+echo 'If the last line is "Lock State : Unlocked", you can continue.'
+read -p "Press enter to continue..."
+flashrecovery
+}
+
+function fastboot {
+echo "Turn on your device and activate USB debuging in Developers Options."
+read -p "Press enter to continue..."
+sudo ./adb devices
+echo "Continue if your device is listed."
+read -p "Press enter to continue..."
+sudo ./adb reboot bootloader
+echo "Continue when your device is in Fastboot mode."
+read -p "Press enter to continue..."
+sudo ./fastboot devices
+echo "Continue if you see your device in list above."
+read -p "Press enter to continue..."
+recovery
+}
+
+function recovery {
+echo "Have you flashed a custom recovery (Select NO if unsure)?"
+echo "1 -- YES"
+echo "2 -- NO"
+choix="Notset"
+echo -n "Type 1 or 2: "
+read choix
+if [ $choix == "1" ]
+then
+	flash
+elif [ $choix == "2" ]
+	flashrecovery
+else
+	echo "Invalid  choice. Type only 1 or 2."
+	recovery
+fi
+}
+
+function flashrecovery {
+echo "Continue to flash custom recovery"
+read -p "Press enter to continue..."
+echo "Flashing recovery.img..."
+sudo ./fastboot flash recovery recovery.img
+echo "If the flash was successful you can continue"
+read -p "Press enter to continue..."
+flash
+}
+
+function flash {
+echo "Now booting to recovery"
+sudo ./fastboot boot recovery.img
+echo "With your recovery, wipe Data, Cache and Dalvik Cache (Under andvanced options) and then start the ADB sideload mode and continue."
+read -p "Press enter to continue..."
+sudo ./adb devices
+echo "Continue if you see your device listed above."
+read -p "Press enter to continue..."
+echo "Flashing cm.zip"
+sudo ./adb sideload cm.zip
+gappschoice
+}
+
+function gappschoice {
+echo "Do you want to flash Google Apps?"
+echo "1 -- YES"
+echo "2 -- NO"
+choix="Notset"
+echo -n "Type 1 or 2: "
+read choix
+if [ $choix == "1" ]
+then
+	if [ ! -f gapps.zip ]
+	then
+		echo "gapps.zip is missing."
+		echo "Please place it in the same directory with this file and continue."
+		read -p "Press enter to continue..."
+		gappschoice
+	fi
+	flashgapps
+elif [ $choix == "2" ]
+	finish
+else
+	echo "Invalid  choice. Type only 1 or 2."
+	gappschoice
+fi
+}
+
+function flashgapps {
+echo "If You got out of ADB sideload mode after the first zip flashed successfully restart sideload mode and continue"
+read -p "Press enter to continue..."
+echo "Flashing gapps.zip"
+sudo ./adb sideload gapps.zip
+finish
+}
+
+function finish {
+echo "COOL! CyanogenMod has been flashed! :)"
+echo "Continue to reboot the device."
+read -p "Press enter to continue..."
+sudo ./adb reboot
+echo "Enjoy your new shiny custom ROM!"
+read -p "Press enter to continue..."
+exit
+}
+
+start
